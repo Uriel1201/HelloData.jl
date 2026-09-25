62d308600c1fbfede81ab0b7a185816666b6dbba
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-09-25 14:06:09 -0600

    Update config.jl

--- src/config.jl
+++ src/config.jl
@@ -1,3 +1,5 @@
+module Config
+
 using DotEnv
 
 env_path = joinpath(@__DIR__, "..", "env")
@@ -8,3 +10,5 @@ const URI_MYSQL = get(ENV, "URI_MYSQL", nothing)
 const ODB_DSN = get(ENV, "ODB_DSN", nothing)
 const ODB_USER = get(ENV, "ODB_USER", nothing)
 const ODB_PASSWORD = get(ENV, "ODB_PASSWORD", nothing)
+
+end #module Config
