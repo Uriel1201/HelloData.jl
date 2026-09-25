e7dd72271d4f019ae17d45227877a5cfa88a22cf
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-09-25 05:48:56 -0600

    Update config.jl

--- src/config.jl
+++ src/config.jl
@@ -1,16 +1,10 @@
-module Config
-
 using DotEnv
 
 env_path = joinpath(@__DIR__, "..", "env")
 DotEnv.load!(env_path)
-
-export URI_POSTGRESQL, URI_MYSQL, ODB_DSN, ODB_USER, ODB_PASSWORD
 
 const URI_POSTGRESQL = get(ENV, "URI_POSTGRESQL", nothing)
 const URI_MYSQL = get(ENV, "URI_MYSQL", nothing)
 const ODB_DSN = get(ENV, "ODB_DSN", nothing)
 const ODB_USER = get(ENV, "ODB_USER", nothing)
 const ODB_PASSWORD = get(ENV, "ODB_PASSWORD", nothing)
-
-end # module Config
