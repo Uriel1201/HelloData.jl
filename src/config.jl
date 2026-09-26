eb974be5b2daf5444bc24032dc1026659489d5d7
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-09-26 01:51:18 -0600

    Fix formatting of environment variable constants

--- src/config.jl
+++ src/config.jl
@@ -1,9 +1,9 @@
 module Config
 
 const URI_POSTGRESQL = get(ENV, "URI_POSTGRESQL", nothing)
-const URI_MYSQL = get(ENV, "URI_MYSQL", nothing)
-const ODB_DSN = get(ENV, "ODB_DSN", nothing)
-const ODB_USER = get(ENV, "ODB_USER", nothing)
-const ODB_PASSWORD = get(ENV, "ODB_PASSWORD", nothing)
+const URI_MYSQL      = get(ENV, "URI_MYSQL", nothing)
+const ODB_DSN        = get(ENV, "ODB_DSN", nothing)
+const ODB_USER       = get(ENV, "ODB_USER", nothing)
+const ODB_PASSWORD.  = get(ENV, "ODB_PASSWORD", nothing)
 
 end #module Config
