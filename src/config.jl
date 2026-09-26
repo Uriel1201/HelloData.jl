88e17199caeca0bfb670aba274aa898147da2fa0
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-09-26 01:16:41 -0600

    Remove module declaration from config.jl

--- src/config.jl
+++ src/config.jl
@@ -1,9 +1,5 @@
-module Config
-
 const URI_POSTGRESQL = ENV["URI_POSTGRESQL"]
 const URI_MYSQL = ENV["URI_MYSQL"]
 const ODB_DSN = ENV["ODB_DSN"]
 const ODB_USER = ENV["ODB_USER"]
 const ODB_PASSWORD = ENV["ODB_PASSWORD"]
-
-end #module Config
