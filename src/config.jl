82a7f15f0cf479455ff1c2fa3015a203e01a664e
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-09-26 01:27:37 -0600

    Update config.jl

--- src/config.jl
+++ src/config.jl
@@ -1,9 +1,7 @@
-function database_config()
-    Dict(
-        :postgresql => ENV["URI_POSTGRESQL"],
-        :mysql => ENV["URI_MYSQL"],
-        :oracle_dsn => ENV["ODB_DSN"],
-        :oracle_user => ENV["ODB_USER"],
-        :oracle_password => ENV["ODB_PASSWORD"],
-    )
-end
+database_config() = Dict(
+    :postgresql => ENV["URI_POSTGRESQL"],
+    :mysql => ENV["URI_MYSQL"],
+    :oracle_dsn => ENV["ODB_DSN"],
+    :oracle_user => ENV["ODB_USER"],
+    :oracle_password => ENV["ODB_PASSWORD"],
+)
